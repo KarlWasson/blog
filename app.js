c6bda91e515aeaa7c5f30dad216b98c1768bf0a1
KarlWasson <18345867+KarlWasson@users.noreply.github.com>
2021-08-15 17:45:42 +0700

    add compose.ejs
    add compose.ejs and button

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.get("/contact", function(req, res){
 app.get("/listings", function(req, res){
   res.render("listings")
 });
+app.get("/compose", function(req, res){
+  res.render("compose")
+});
 
 
 
